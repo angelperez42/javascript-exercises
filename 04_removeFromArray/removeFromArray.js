12ce856944f203f42942de9fbe132e2cb91cf32a
Angel Perez <101531336+angelperez42@users.noreply.github.com>
2024-07-06 22:16:07 -0400

    feat: Create a function to remove argum from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = array.length-1; i >= 0; i--) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (array[i] === arguments[j]) {
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
 };
+
+// const removeFromArray = function(array, ...args) {
+//     const newArray = [];
+//     array.forEach((item) => {
+//         if (!args.includes(item)) {
+//             newArray.push(item);
+//         }
+//     });
+//     return newArray;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
