6e3f1e17efeb65276206404b40fee0c047dff435
Angel Perez <101531336+angelperez42@users.noreply.github.com>
2024-07-06 22:16:52 -0400

    feat: Create a function to sum between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || typeof a == 'string' || typeof b == 'string') {
+        return 'ERROR';
+    }   else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    }   else if (a > b) {
+            for (let j = b; j <= a; j++) {
+                sum += j;
+            }
+    }   else {
+        return 'ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
